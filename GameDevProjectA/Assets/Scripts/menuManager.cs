d2612406add8d4f0bb1fe3cdb42f3a81a2ca4c67
Samwise0000 <ben@dream-front.com>
2020-09-18 11:11:43 -0600

    Commit just finished building menu function / can start game, two camaers, switching OK, size issue reolved.

---
+++ GameDevProjectA/Assets/Scripts/menuManager.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menuManager : MonoBehaviour
+{
+    // Create public variables (GameObjects)
+    public GameObject player;
+    public GameObject mainMenu;
+    public GameObject curser;
+    public GameObject level01;
+    public GameObject[] curserPositions;
+    public GameObject playerSelectIndicator;
+    public Text thisText;
+
+    public Camera gameCamera;
+    public Camera mainCamera;
+
+    private bool onStartGameSelection;
+    private bool onePlayer = true;
+
+    //int thisNumber = 0;
+    //float thisDecimalNumber = 0.5f;
+    //string yourName = "Harry Potter";
+    //bool yourValue = true; // (or false)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        onStartGameSelection = false;
+        thisText = playerSelectIndicator.GetComponent<Text>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for space or enter key pressed
+        // if pressed then deactivate the menu game object
+        // and show the player character game object
+
+        //if (Input.GetKey("space") || Input.GetKey("return"))
+        //{
+        //    player.SetActive(true);
+        //    mainMenu.SetActive(false);
+        //}
+
+        // if the user presses up
+        // move curser to next selection
+
+        if(Input.GetKeyDown("up") || Input.GetKeyDown("down"))
+        {
+            if(onStartGameSelection)
+            {
+                onStartGameSelection = false;
+            }
+            else
+            {
+                onStartGameSelection = true;
+            }
+        }
+
+        if(onStartGameSelection)
+        {
+            curser.transform.position = curserPositions[1].transform.position;
+        }
+        else
+        {
+            curser.transform.position = curserPositions[0].transform.position;
+        }
+
+        if(onStartGameSelection && Input.GetKeyDown("return"))
+        {
+            // Start game
+            level01.SetActive(true);
+            player.SetActive(true);
+            mainMenu.SetActive(false);
+
+            mainCamera.enabled = false;
+            gameCamera.enabled = true;
+        }
+
+        // Toggle number of players
+        if (!onStartGameSelection && Input.GetKeyDown("return"))
+        {
+            if (onePlayer)
+            {
+                thisText.text = "2";
+                onePlayer = false;
+            }
+            else
+            {
+                thisText.text = "1";
+                onePlayer = true;
+            }
+        }
+
+        // if user presses down
+        // move curser to next selection
+
+        // if the user presses enter AND if onStartGameSelection is false then
+        // toggle number of players
+
+        // if the user presses enter AND if onStartGameSelection is true then
+        // start the game
+
+    }
+}
---
+++ GameDevProjectA/Assets/Scripts/playerManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerManager : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    SpriteRenderer spriteRen;
+    Animator tankAnim;
+
+    // declare variables 
+    // we need a ridgidbody reference
+    // we need a sprite renderer reference
+    // and we need an animator reference
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Show text that class is running
+        Debug.Log("Test: Player Manager reporting!");
+
+        // Assign/initializing the variables
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRen = GetComponent<SpriteRenderer>();
+        tankAnim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // get user input
+
+        // if user pushes up key, then move gameobject up
+        // check for up key pressed
+        // change rigid body velocity (0, 2)
+        // play animation: PlayerMoveA
+
+        // if user pushes the down key, move gameobject down
+        // check for down key pressed
+        // change rigid body velocity (0, -2)
+        // flip sprite on y
+        // play animation: PlayerMoveA
+
+        // if user pushes right, move right
+        // check for right arrow pressed
+        // change rigid body velocity (2,0)
+        // play animation: PlayerMoveB
+
+        // if user pushes left, move left
+        // check for left arrow pressed
+        // change rigid body velocity (-2,0)
+        // flip sprite on x
+        // play animation: PlayerMoveB
+
+        // Deal with user input
+        if (Input.GetKey("w") || Input.GetKey("up"))
+        {
+            rb2d.velocity = new Vector2(0, 2);
+            spriteRen.flipY = false;
+            tankAnim.Play("PlayerMoveA");
+        }
+        else if(Input.GetKey("s") || Input.GetKey("down"))
+        {
+            rb2d.velocity = new Vector2(0, -2);
+            spriteRen.flipY = true;
+            tankAnim.Play("PlayerMoveA");
+        }
+        else if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2d.velocity = new Vector2(2, 0);
+            spriteRen.flipX = false;
+            tankAnim.Play("PlayerMoveB");
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2d.velocity = new Vector2(-2, 0);
+            spriteRen.flipX = true;
+            tankAnim.Play("PlayerMoveB");
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(0, 0);
+        }
+
+    }
+}
